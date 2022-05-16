3b7f1cf3b97b2c612927a53575f0cf4ff2704bbd
RodrigoJScript <rodrigo777.rjp@gmail.com>
2022-05-16 13:27:01 -0500

    Creado un dropdown personalizado

--- app/src/main/java/com/rodrigojscript/coffee4coders/ui/screens/CheckoutScreen.kt
+++ app/src/main/java/com/rodrigojscript/coffee4coders/ui/screens/CheckoutScreen.kt
@@ -1,16 +1,31 @@
 package com.rodrigojscript.coffee4coders.ui.screens
 
+import androidx.compose.material.DropdownMenu
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import com.rodrigojscript.coffee4coders.ui.components.CustomDropdownTextField
 import com.rodrigojscript.coffee4coders.ui.components.CustomTextField
 import com.rodrigojscript.coffee4coders.ui.theme.BaseAppTheme
 
 @Composable
 fun CheckoutScreen() {
-    var name by remember { mutableStateOf("") }
+    var city by remember { mutableStateOf("") }
+    val cities = listOf<String>(
+        "All",
+        "children",
+        "like",
+        "sliced",
+        "lobsters",
+        "in",
+        "whiskey",
+        "and",
+        "chipotle",
+        "chile",
+        "powder"
+    )
 
-    CustomTextField(value = name, placeholder = "Nombre") {
-        name = it
+    CustomDropdownTextField(suggestions = cities, value = city, placeholder = "Ciudad") {
+        city = it
     }
 }
 
