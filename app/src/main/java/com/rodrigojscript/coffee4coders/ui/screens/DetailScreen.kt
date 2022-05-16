2fc6f56e4b4df9bff1a5e4816722fc7ba55a7fa4
RodrigoJScript <rodrigo777.rjp@gmail.com>
2022-05-16 09:41:05 -0500

    pantalla de detalles terminada

--- app/src/main/java/com/rodrigojscript/coffee4coders/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/rodrigojscript/coffee4coders/ui/screens/DetailScreen.kt
@@ -1,6 +1,9 @@
 package com.rodrigojscript.coffee4coders.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -8,11 +11,17 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.rodrigojscript.coffee4coders.ui.components.CountryISO
-import com.rodrigojscript.coffee4coders.ui.components.CustomAppBar
+import com.rodrigojscript.coffee4coders.R
+import com.rodrigojscript.coffee4coders.ui.components.*
+import com.rodrigojscript.coffee4coders.ui.theme.BaseAppTheme
 
 @Composable
 fun DetailScreen(navController: NavController, countryISO: CountryISO) {
@@ -25,17 +34,41 @@ fun DetailScreen(navController: NavController, countryISO: CountryISO) {
             }
         },
         content = {
-            Column {
-                Text(
-                    text = "Soy un producto de: ${countryISO.iso}",
-                    style = MaterialTheme.typography.h3
-                )
-                Button(onClick = {
-                    navController.navigate("feed") {
-                        popUpTo("feed")
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = countryISO.getBackgroundImage()),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Column(modifier = Modifier.padding(16.dp)) {
+                    TitleText(title = "Cafe de Colombia")
+                    Text(
+                        text = "Red alert, senior alarm! The dubloon hauls with fortune, command the pacific ocean. All children like cooked chickpeas in beer and cinnamon. The brilliant mind is full of reincarnation.Classis moris, tanquam germanus homo.",
+                        style = MaterialTheme.typography.caption
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                    BodyText(text = "The addled cannon calmly raids the corsair. Assimilant satis ducunt ad alter fraticinida. The honor is a remarkable species.")
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        Text(
+                            text = "$ 35.0 USD",
+                            style = MaterialTheme.typography.h5,
+                            textAlign = TextAlign.End
+                        )
+                        CustomButton(label = "Continuar") {
+                            navController.navigate("checkout") {
+                                launchSingleTop = true
+                            }
+                        }
                     }
-                }) {
-                    Text(text = "Volver")
+
                 }
             }
         }
@@ -45,6 +78,8 @@ fun DetailScreen(navController: NavController, countryISO: CountryISO) {
 @Preview(showBackground = true)
 @Composable
 fun DetailScreenPreview() {
-    val navController = rememberNavController()
-    DetailScreen(navController, CountryISO.BRA)
+    BaseAppTheme {
+        val navController = rememberNavController()
+        DetailScreen(navController, CountryISO.BRA)
+    }
 }
